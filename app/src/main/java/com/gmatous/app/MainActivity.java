e25bee9ae3a10f809863b92345a82848afb52daa
gmatous <gregory.matous@gmail.com>
2013-12-25 23:55:53 -0600

    master

--- app/src/main/java/com/gmatous/app/MainActivity.java
+++ app/src/main/java/com/gmatous/app/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
-// send me to dev
+// local dev
 public class MainActivity extends Activity {
 
     @Override
