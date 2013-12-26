9c0c7206e29d7c79e158d33dc9507e424e845814
gmatous <gregory.matous@gmail.com>
2013-12-25 23:16:14 -0600

    localdev

--- app/src/main/java/com/gmatous/app/MainActivity.java
+++ app/src/main/java/com/gmatous/app/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
-// what branch?
+// send me to dev
 public class MainActivity extends Activity {
 
     @Override
